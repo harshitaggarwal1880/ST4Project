adc387ea9aee1c843cb2e780ec2e2bedfbccd8f5
Harsh <harsh@gmail.com>
2022-10-05 13:23:45 +0530

    password hashinng and jwt auth token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,6 +4,18 @@ const router = express.Router()
 
 const User = require("../models/User")
 
+// bcryptjs is a node package for hashing password with bcrypt generated salt and pepper
+// password hasing - is a one way function to hash/encrpt a password with different algorithm and impossible to convert back into plain text
+// salt - salt is a addition data added to password text to make a password not common and then hashing that password to make password unable to hack
+// pepper - pepper is also like salt comes after salt and pepper is hardcore value in backend
+const bcrypt = require('bcryptjs');
+
+
+// jwt authentication 
+// jwt is a node package for jwt (json web token) authentication 
+const jwt = require('jsonwebtoken');
+const JWT_SECRET_KEY = "shhhhhh";
+
 
 router.use('/authall/', require('./authall'));
 
@@ -13,6 +25,7 @@ router.use('/authall/', require('./authall'));
 
 const { body, validationResult} = require('express-validator');
 
+// create a new user (store new data of user into mongodb)
 router.post('/createuser', [ 
     body('email','Enter the valid Email').isEmail(),
     body('name','Enter the valid Name').isLength({ min:5 }),
@@ -32,16 +45,31 @@ router.post('/createuser', [
         return res.status(400).json({error: "User with Same Email exits already", UserAlready: user})
     }
 
-
+    // here we generate a salt from bcrypt package
+    const salt = await bcrypt.genSalt(10)
+    
+    // here we hash a user password with salt 
+    const Securedpass = await bcrypt.hash(req.body.password, salt);
 
     user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: Securedpass    // here we store hash passwrod in mongodb 
     })
 
-    res.json(user);
+    const data = {
+        user: {
+            id: user.id
+        }
     }
+
+    // here we generate a json web token by signing with secret key using jwt package and here we using mongodb user document id as data, so that id retrieval from db is efficient.
+    const jwtToken = jwt.sign(data,JWT_SECRET_KEY);
+
+    // we sending jwt token to user
+    res.json({jwtToken});
+    }
+
     catch(error){
         console.log(error.message)
         res.status(500).send("Something went Wrong");
